6f400f5c246eb985b707809b2a3a0136b05cf194
Stephano55 <kevinstephano14@gmail.com>
2024-04-27 14:00:45 +0200

    creation de procédures

--- main.c
+++ main.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+void allouer(int **tableau,int taille){
+    *tableau = (int *)malloc(taille * sizeof(int));
+}
+void remplir(int *tableau, int taille){
+    printf("entrez les elements du tableau : \n");
+    for(int i=0; i < taille; i++){
+        scanf("%d", &tableau[i]);
+    }
+}
+void afficher(int *tableau, int taille){
+    for(int i=0; i < taille; i++){
+        printf("%d ", tableau[i]);
+    }
+}
 void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
-int main()
-{
-    int *tableau;
-    int taille;
-    printf("Entrez la taille du tableau: ");
-    scanf("%d", &taille);
-    tableau = (int *)malloc(taille * sizeof(int));
-    printf("entrez les elements du tableau : \n");
-    for(int i=0; i < taille; i++){
-        scanf("%d", &tableau[i]);
-    }
-    printf("tableau avant la melange :\n");
-    for(int i=0; i < taille; i++){
-        printf("%d ", tableau[i]);
-    }
+void melanger(int *tableau, int taille){
     if (taille > 1) {
         srand(time(NULL)); // Initialisation de la graine de génération aléatoire
         for (int i = taille - 1; i > 0; i--) {
@@ -27,9 +28,20 @@ int main()
             swap(&tableau[i], &tableau[j]); // Échange des éléments aux positions i et j
         }
     }
+}
+int main()
+{
+    int *tableau;
+    int taille;
+    printf("Entrez la taille du tableau: ");
+    scanf("%d", &taille);
+    allouer(&tableau, taille);
+    remplir(tableau, taille);
+    printf("tableau avant la melange :\n");
+    afficher(tableau, taille);
+    melanger(tableau, taille);
     printf("\nApres melange : \n");
-    for(int i=0; i < taille; i++){
-        printf("%d ", tableau[i]);
-    }
+    afficher(tableau, taille);
+    free(tableau);
     return 0;
 }
